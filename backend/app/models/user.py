a0086b3203b5bb0ef7e7761820ccff6c70c85c2d
TrinkeshKonverge <trinkesh.nimasarkar@konverge.ai>
2026-08-07 15:20:33 +0530

    Complete authentication module and update core, models,schema and logs

---
+++ backend/app/models/user.py
@@ -0,0 +1,28 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+
+from app.database.database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    user_id = Column(Integer, primary_key=True, index=True)
+
+    name = Column(String, nullable=False)
+
+    email = Column(String, unique=True, nullable=False, index=True)
+
+    password_hash = Column(String, nullable=False)
+
+    role = Column(String, nullable=False)
+
+    firm_id = Column(Integer, ForeignKey("firms.firm_id"), nullable=True)
+
+    # Relationships
+    firm = relationship("Firm", back_populates="users")
+
+    audit_logs = relationship(
+        "AuditLog",
+        back_populates="user"
+    )
